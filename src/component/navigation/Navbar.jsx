4cfd2a3ee2e97952f880465c0198fff49eac8947
Yunus Abbas Opeyemi <opeyemiakanbi328@gmail.com>
2023-08-19 18:44:38 +0100

    navbar container padding removed at 2xl screen breakpoint

--- src/component/navigation/Navbar.jsx
+++ src/component/navigation/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className='fixed top-0 left-0 right-0 bg-white z-20 h-[7rem] dark:bg-very-dark-blue-DMB shadow-sm dark:shadow-dark-blue-dark-DME'>
-      <div className=' h-full px-6 mx-auto flex gap-6 items-center justify-between max-w-[120rem]'>
+      <div className=' h-full px-6 2xl:px-0 mx-auto flex gap-6 items-center justify-between max-w-[120rem]'>
         <header>
           <h1 className='text-3xl font-semibold text-dark-blue-dark-LMT dark:text-white'>Where in the world?</h1>
         </header>
